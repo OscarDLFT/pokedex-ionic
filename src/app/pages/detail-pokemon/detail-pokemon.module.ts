72ddd3bebd051ece0e7cda735ac396b745499213
Oscar <oscardelafuentetoledo10@gmail.com>
2023-07-03 18:14:36 +0200

    añadidas rutas de navegacion

--- src/app/pages/detail-pokemon/detail-pokemon.module.ts
+++ src/app/pages/detail-pokemon/detail-pokemon.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DetailPokemonComponent } from './detail-pokemon.component';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const rutas: Routes = [
+  {
+    path: '',
+    component: DetailPokemonComponent
+  }
+]
 
 @NgModule({
   declarations: [DetailPokemonComponent],
-  imports: [
-    CommonModule
-  ],
   exports: [DetailPokemonComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(rutas), 
+    FormsModule,
+    IonicModule
+  ],
 })
 export class DetailPokemonModule { }
--- src/app/pages/list-pokemons/list-pokemons.module.ts
+++ src/app/pages/list-pokemons/list-pokemons.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ListPokemonsComponent } from './list-pokemons.component';
+import { IonicModule } from '@ionic/angular';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const rutas: Routes = [
+  {
+    path: '',
+    component: ListPokemonsComponent
+  }
+]
 
 @NgModule({
-  declarations: [],
+  declarations: [ListPokemonsComponent],
+  exports: [ListPokemonsComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(rutas), 
+    FormsModule,
+    IonicModule
   ]
 })
 export class ListPokemonsModule { }
